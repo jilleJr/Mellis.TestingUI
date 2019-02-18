91f36b91f0f607d54fddafea850d94c2b3b5d991
kalle (jag) <kalle.jillheden@gmail.com>
2019-02-18 12:42:29 +0100

    Setup main window structure

---
+++ Assets/Scripts/InputColorTheCodeYo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using PM;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputColorTheCodeYo : MonoBehaviour
+{
+    //public TMP_InputField input;
+    public TMP_Text output;
+
+    public void UpdateColoredOutput(string text)
+    {
+        output.text = IDEColorCoding.runColorCode(text);
+    }
+}
